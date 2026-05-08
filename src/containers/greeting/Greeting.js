9d6de16ac4c6000e23b7b40b2ff9244dfe93493d
SarathKumar Krishna Sha <ksarath429@yahoo.com>
2026-05-08 00:18:10 -0400

    remove waving hand

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -1,37 +1,40 @@
 import React, {useContext} from "react";
 import {Fade} from "react-reveal";
-import emoji from "react-easy-emoji";
 import "./Greeting.scss";
 import SocialMedia from "../../components/socialMedia/SocialMedia";
 import {greeting} from "../../portfolio";
 import StyleContext from "../../contexts/StyleContext";
-console.log("test");
 
 export default function Greeting() {
   const {isDark} = useContext(StyleContext);
+
   if (!greeting.displayGreeting) {
     return null;
   }
+
   return (
     <Fade bottom duration={1000} distance="40px">
       <div className="greet-main" id="greeting">
         <div className="greeting-main">
           <div
-  className="greeting-text-div"
-  style={{
-    width: "100%",
-    textAlign: "center",
-    margin: "0 auto"
-  }}
->
+            className="greeting-text-div"
+            style={{
+              width: "100%",
+              textAlign: "center",
+              margin: "0 auto"
+            }}
+          >
             <div>
               <h1
-                className={isDark ? "dark-mode greeting-text" : "greeting-text"}
+                className={
+                  isDark
+                    ? "dark-mode greeting-text"
+                    : "greeting-text"
+                }
               >
-                {" "}
-                {greeting.title}{" "}
-                <span className="wave-emoji">{emoji("👋")}</span>
+                {greeting.title}
               </h1>
+
               <p
                 className={
                   isDark
@@ -41,12 +44,12 @@ export default function Greeting() {
               >
                 {greeting.subTitle}
               </p>
+
               <div id="resume" className="empty-div"></div>
+
               <SocialMedia />
-             
             </div>
           </div>
-        
         </div>
       </div>
     </Fade>
